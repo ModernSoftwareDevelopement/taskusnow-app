de89c42ddba02e0aa119329e7b76e60353b1cc6d
thet naing <fatalthetnaing@gmail.com>
2023-09-12 17:29:07 +0800

    feature: added zod for validation in form, used react hook form

--- src/pages/applications/Task/components/TaskForm.tsx
+++ src/pages/applications/Task/components/TaskForm.tsx
@@ -1,51 +1,78 @@
 import "./TaskForm.css";
-import { FormEvent, useRef } from 'react';
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FieldValues, useForm } from "react-hook-form";
 import { postTask } from "../../../../services/TaskAPI";
 
+
+const schema = z.object({
+  title: z
+    .string()
+    .min(3, { message: "Title must contain at least 3 characters!" }),
+  description: z.string().min(10),
+}); // creating zod schema for variable, data type, limit and error message
+
+type TaskInterface = z.infer<typeof schema>; // to create shape of Task interface from zod base on schema
+
 const TaskForm = () => {
-  const titleRef = useRef<HTMLInputElement>(null);
-  const descriptionRef = useRef<HTMLTextAreaElement>(null);
-  const task = { title: '', description: '', userid: 123}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<TaskInterface>({ resolver: zodResolver(schema) });
 
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
-    if(titleRef.current !== null)
-    task.title= titleRef.current.value;
+  console.log(errors);
 
-    if(descriptionRef.current !== null)
-    task.description = descriptionRef.current.value;
-    
-    console.log(task);
-
+  const onSubmit = async (data: FieldValues) => {
+    console.log(data);
+    const task = {
+      title: data.title,
+      description: data.description,
+      userid: 123,
+    };
     try {
       await postTask(task);
-      console.log('Task submitted successfully');
+      alert("Task created!");      
+      reset();
     } catch (error) {
-      console.error('An error occurred on task submission:', error);
+      console.error("Error creating task:", error);
+      alert("Failed to create task. Please try again.");      
     }
-  }
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="container">
         <div className="row">
           <div className="col-md-4 offset-md-4">
-            <div className="mb-3">
+            <div className="mb-3">              
               <label htmlFor="title" className="form-label">
                 Title
               </label>
-              <input ref={titleRef} id="title" type="text" className="form-control" />
+              <input
+                {...register("title")}
+                id="title"
+                type="text"
+                className="form-control"
+              />
+              {errors.title && (
+                <p className="text-danger">{errors.title.message}</p>
+              )}
             </div>
             <div className="mb-3">
               <label htmlFor="description" className="form-label">
                 Description
               </label>
               <textarea
-                ref={descriptionRef}
+                {...register("description")}
                 id="description"
                 rows={5}
                 className="form-control"
               ></textarea>
+              {errors.description && (
+                <p className="text-danger">{errors.description.message}</p>
+              )}
             </div>
             <button className="btn btn-primary" type="submit">
               Submit
