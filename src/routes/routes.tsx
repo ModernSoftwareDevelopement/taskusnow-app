bc65fe3caa593aa6e29f36a6e3cf111ba0273663
thetnaingtta <fatalthetnaing@gmail.com>
2023-11-29 08:16:13 +0800

    chore: plannning for api client

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,37 +5,44 @@ import PublicRoutes from './PublicRoutes.tsx';
 import Dashboard from '../pages/dashboards/Dashboard.tsx';
 import Home from '../pages/home/Home.tsx';
 import UserProfilePage from '../pages/applications/Users/UserProfilePage.tsx';
+import Tasks from '../pages/applications/Tasks/components/Tasks.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <PublicRoutes/>,
+    element: <PublicRoutes />,
     children: [
       {
-        index: true, element: <Home/>
+        index: true,
+        element: <Home />,
       },
       {
-        path: 'login', element: <LoginPage/>
+        path: 'login',
+        element: <LoginPage />,
       },
       {
-        path: 'about', element: <div>About</div>
-      }
-    ]
+        path: 'about',
+        element: <div>About</div>,
+      },
+    ],
   },
   {
-    element: <PrivateRoutes/>,
+    element: <PrivateRoutes />,
     children: [
       {
-        path: 'dashboard', element: <Dashboard/>
+        path: 'dashboard',
+        element: <Dashboard />,
       },
       {
-        path: 'profile', element: <UserProfilePage/>
+        path: 'profile',
+        element: <UserProfilePage />,
       },
       {
-        path: 'tasks', element: <div>Tasks</div>
+        path: 'tasks',
+        element: <Tasks />,
       },
-    ]
-  }
-])
+    ],
+  },
+]);
 
 export default router;
